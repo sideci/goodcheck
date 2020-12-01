3ff013f1a0c3a23b84c4befb0130a7375ab9fa8d
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2020-12-02 00:39:22 +0900

    Fix `Security/YAMLLoad` issue
    https://sider.review/gh/repos/112739662/branches/master/commits/4145e6a2ab0a27091fe70fadffdf4174adb6c917#issue-105158491

--- lib/goodcheck/commands/config_loading.rb
+++ lib/goodcheck/commands/config_loading.rb
@@ -21,7 +21,7 @@ module Goodcheck
           end
 
         import_loader = ImportLoader.new(cache_path: cache_path, force_download: force_download, config_path: config_path)
-        content = JSON.parse(JSON.dump(YAML.load(config_content, filename: config_path.to_s)), symbolize_names: true)
+        content = JSON.parse(JSON.dump(YAML.safe_load(config_content, filename: config_path.to_s)), symbolize_names: true)
         loader = ConfigLoader.new(path: config_path, content: content, stderr: stderr, import_loader: import_loader)
         @config = loader.load
       end
