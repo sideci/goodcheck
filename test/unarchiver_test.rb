dea519bc7fccbd12b5857585d2812b8868429a51
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2021-06-02 22:57:38 +0900

    Exclude non-YAML files from `*.tar.gz`
    Follow-up of #190

--- test/unarchiver_test.rb
+++ test/unarchiver_test.rb
@@ -6,6 +6,8 @@ class UnarchiverTest < Minitest::Test
   SAMPLE_FILE = Pathname(__dir__) / "fixtures" / "goodcheck-test-rules.tar.gz"
 
   def test_tar_gz?
+    subject = Unarchiver.new
+
     assert subject.tar_gz?("a.tar.gz")
     assert subject.tar_gz?("a/b.tar.gz")
     assert subject.tar_gz?(Pathname("a/b.tar.gz"))
@@ -17,18 +19,23 @@ class UnarchiverTest < Minitest::Test
 
   def test_tar_gz
     block_args = []
-    subject.tar_gz(SAMPLE_FILE.read) do |content, filename|
+    Unarchiver.new.tar_gz(SAMPLE_FILE.read) do |content, filename|
       block_args << [content, filename]
     end
 
-    assert_equal ["rules/a.yml", "rules/sub/b.yaml"], block_args.map(&:last)
+    assert_equal ["rules/a.yml", "rules/goodcheck.yml", "rules/foo.txt", "rules/sub/b.yaml"], block_args.map(&:last)
     assert_match "- id: rule.a", block_args[0][0]
-    assert_match "- id: rule.b", block_args[1][0]
+    assert_match "- id: rule.b", block_args[3][0]
   end
 
-  private
+  def test_tar_gz_with_filter
+    subject = Unarchiver.new(file_filter: ->(filename) { filename.end_with?(".yaml") })
 
-  def subject
-    @subject ||= Unarchiver.new
+    filenames = []
+    subject.tar_gz(SAMPLE_FILE.read) do |_content, filename|
+      filenames << filename
+    end
+
+    assert_equal ["rules/sub/b.yaml"], filenames
   end
 end
